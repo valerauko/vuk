5fb7fc3d01341b9cec1481034cc7834bafb8320e
Unknown <valerauko@gmail.com>
2018-05-06 20:20:29 +0900

    Add request functions to core

--- src/vuk/core.clj
+++ src/vuk/core.clj
@@ -1,6 +1,16 @@
-(ns vuk.core)
+(ns vuk.core
+  (:require [vuk.request :as request]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn finger
+  "The main function to look up an 'acct'."
+  ([acct] (finger acct {}))
+  ([acct options]
+    (request/lookup acct options)))
+
+(defn link
+  ; REVIEW: i'm assuming that rels are supposed to be unique.
+  ; if there can be multiple of the same rel, this should be changed
+  ; to return an array of matching rels
+  "Extracts a link from a finger result based on its 'rel'."
+  [result rel]
+  (->> result :links (filter #(= (:rel %) rel)) first))
