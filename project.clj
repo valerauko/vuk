77b604e02da0943bcc88b38a8bf0c20b52ba7e8b
Vale <valerauko@gmail.com>
2018-10-10 03:13:52 +0900

    Replace clj-http with aleph

--- project.clj
+++ project.clj
@@ -6,5 +6,4 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.xml "0.0.8"]
                  [metosin/jsonista "0.2.1"]
-                 [clj-http "3.9.0"]]
-  :profiles {:dev {:dependencies [[clj-http-fake "1.0.3"]]}})
+                 [aleph "0.4.6"]])
