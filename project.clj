d579f08e7e143103069d9ff26a41ad7bcc3705b0
Vale <valerauko@gmail.com>
2023-10-04 15:09:50 +0900

    Add github action testing

--- project.clj
+++ project.clj
@@ -3,7 +3,17 @@
   :url "http://github.com/valerauko/vuk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/data.xml "0.0.8"]
+  :dependencies [[org.clojure/data.xml "0.0.8"]
                  [metosin/jsonista "0.2.7"]
-                 [aleph "0.4.6"]])
+                 [aleph "0.4.6"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]
+                   :global-vars {*warn-on-reflection* true
+                                 *unchecked-math* :warn-on-boxed}
+                   :plugins [[lein-ancient "0.7.0"
+                              :exclusions [org.clojure/clojure]]]}
+             :clj1.9.0 {:dependencies
+                        [[org.clojure/clojure "1.9.0"]]}
+             :clj1.10.3 {:dependencies
+                         [[org.clojure/clojure "1.10.3"]]}
+             :clj1.11.1 {:dependencies
+                         [[org.clojure/clojure "1.11.1"]]}})
