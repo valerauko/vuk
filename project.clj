7e31aea7b2e1d1f414f15adc617cdc1f2e05b63e
Vale <valerauko@gmail.com>
2023-10-04 15:36:08 +0900

    Update deps and bump version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject social.kitsune/vuk "0.2.1"
+(defproject social.kitsune/vuk "0.2.2"
   :description "A Clojure library to handle WebFinger interactions for you."
   :url "http://github.com/valerauko/vuk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
   :dependencies [[org.clojure/data.xml "0.0.8"]
-                 [metosin/jsonista "0.2.7"]
-                 [aleph "0.4.6"]]
+                 [metosin/jsonista "0.3.8"]
+                 [aleph "0.6.3"]]
   :deploy-repositories {"clojars" {:url "https://repo.clojars.org/"
                                    :username :env/clojars_user
                                    :password :env/clojars_token}}
