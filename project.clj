064e478ca8dcb6fc3cdb9b30b0a1fc4ebbdd1ba1
Vale <valerauko@gmail.com>
2025-02-06 22:20:37 +0900

    Bump

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject social.kitsune/vuk "0.2.2"
+(defproject social.kitsune/vuk "0.2.3"
   :description "A Clojure library to handle WebFinger interactions for you."
   :url "http://github.com/valerauko/vuk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
   :scm {:name "git"
         :url "https://github.com/valerauko/vuk"
-        :tag "v0.2.2"}
+        :tag "v0.2.3"}
   :dependencies [[org.clojure/data.xml "0.0.8"]
                  [metosin/jsonista "0.3.13"]
                  [aleph "0.8.2"]]
