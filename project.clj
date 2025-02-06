52215fff5ce73e6d20b1f6043334ed7ee63c0f9e
Vale <valerauko@gmail.com>
2025-02-06 21:49:10 +0900

    Update deps

--- project.clj
+++ project.clj
@@ -7,12 +7,12 @@
         :url "https://github.com/valerauko/vuk"
         :tag "v0.2.2"}
   :dependencies [[org.clojure/data.xml "0.0.8"]
-                 [metosin/jsonista "0.3.8"]
-                 [aleph "0.6.3"]]
+                 [metosin/jsonista "0.3.13"]
+                 [aleph "0.8.2"]]
   :deploy-repositories {"clojars" {:url "https://repo.clojars.org/"
                                    :username :env/clojars_user
                                    :password :env/clojars_token}}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.12.0"]]
                    :global-vars {*warn-on-reflection* true
                                  *unchecked-math* :warn-on-boxed}
                    :plugins [[lein-ancient "0.7.0"
