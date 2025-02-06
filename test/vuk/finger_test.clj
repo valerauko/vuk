77aeafc0096ec7dddb1381221490022fc431e746
Vale <valerauko@gmail.com>
2025-02-06 22:00:19 +0900

    Fix weirdness in test ns

--- test/vuk/finger_test.clj
+++ test/vuk/finger_test.clj
@@ -1,6 +1,7 @@
 (ns vuk.finger-test
-  (:require [clojure.test :refer :all]
-            [vuk.core :refer :all]))
+  (:require [aleph.http]
+            [clojure.test :refer [deftest testing is]]
+            [vuk.core :refer [finger]]))
 
 (def user-acct
   "acct:valerauko@pawoo.net")
@@ -8,20 +9,18 @@
 (def user-url
   "https://pawoo.net/authorize_follow?acct=valerauko@pawoo.net")
 
-(with-redefs
-  {#'aleph.http/get (fn [_uri]
-                      {:status 200
-                       :headers {:content-type "application/xml"}
-                       :body (slurp "fixtures/webfinger.xml")})}
+(with-redefs [aleph.http/get (fn [_uri]
+                               {:status 200
+                                :headers {:content-type "application/xml"}
+                                :body (slurp "fixtures/webfinger.xml")})]
   (deftest xml-test
     (testing "Can handle XML responses"
       (is (= user-acct (-> user-acct finger :subject))))))
 
-(with-redefs
-  {#'aleph.http/get (fn [_uri]
-                      {:status 200
-                       :headers {:content-type "application/json"}
-                       :body (slurp "fixtures/webfinger.json")})}
+(with-redefs [aleph.http/get (fn [_uri]
+                               {:status 200
+                                :headers {:content-type "application/json"}
+                                :body (slurp "fixtures/webfinger.json")})]
   (deftest json-test
     (testing "Can handle JSON responses"
       (is (= user-acct (-> user-acct finger :subject))))))
